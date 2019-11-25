a129e503814eb743397818a9db652ac63edd25b6
Hirbod Behnam <chrome.hiri.angry@gmail.com>
2019-11-25 19:14:02 +0330

    Added date validator

--- MTProtoProxyInstall.sh
+++ MTProtoProxyInstall.sh
@@ -171,8 +171,7 @@ if [ -d "/opt/mtprotoproxy" ]; then
 		echo "$SECRET" >>tempSecrets.json
 		SECRET_ARY=()
 		mapfile -t SECRET_ARY < <(jq -r 'keys[]' tempSecrets.json)
-		#Check secure only and stuff
-		TLS=false
+		#Print
 		for user in "${SECRET_ARY[@]}"; do
 			SECRET=$(jq --arg u "$user" -r '.[$u]' tempSecrets.json)
 			s=$(python3.6 -c "print(\"ee\" + \"$SECRET\" + \"$TLS_DOMAIN\".encode().hex())")
@@ -390,6 +389,14 @@ if [ -d "/opt/mtprotoproxy" ]; then
 		else
 			ListUsersAndSelect
 			read -r -p "Enter the expiry date in format of day/month/year(Example 11/9/2019). Enter nothing for removal: " DATE
+		fi
+		if ! [[ $DATE =~ ^[0-9]{2}/[0-9]{2}/[0-9]{4}$ ]]; then 
+			if [ "$#" -ge 2 ]; then
+				PrintErrorJson "Invalid date format"
+			else
+				echo "Invalid format (DD/MM/YYYY)"
+				exit 1
+			fi
 		fi
 		if [[ $DATE == "" ]]; then
 			j=$(jq -c --arg k "$KEY" 'del(.[$k])' limits_date.json)
